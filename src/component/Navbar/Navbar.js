d3c71f517f7ff115c232ea37e3ae2e3e7797ed4d
Sahana Balram <sahana2008balram@gmail.com>
2018-04-27 14:43:49 -0700

    Navbar component

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import "./Navbar.css";
+
+class NavbarContainer extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default NavbarContainer;
