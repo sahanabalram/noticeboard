79dd4ed08da064a5fdf27c0b4d17cc16c135b1b2
Sahana Balram <sahana2008balram@gmail.com>
2018-04-27 14:52:58 -0700

    Add navbar links

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,10 +1,22 @@
 import React,{Component} from "react";
+import {Navbar} from "react-bootstrap";
 import "./Navbar.css";
 
 class NavbarContainer extends Component{
     render(){
         return(
-            <div></div>
+            <Navbar className="top-navbar">
+            <Navbar.Header>
+                <Navbar.Brand>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Navbar.Text pullRight>
+                <Navbar.Link href="#about">About</Navbar.Link>
+                <Navbar.Link href="#categories">Categories</Navbar.Link>
+                <Navbar.Link href="#login">Login</Navbar.Link>
+                <Navbar.Link href="#register">Register</Navbar.Link>
+            </Navbar.Text>
+        </Navbar>
         )
     }
 }
